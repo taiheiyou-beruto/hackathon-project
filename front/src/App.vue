c45ab212d75bb5c5bb3098b4ad45d285e2157b50
saito yuto <57883503+mercury-pluto1210@users.noreply.github.com>
2021-02-03 13:48:48 +0900

    add front

---
+++ front/src/App.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="body">
+    <header>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="title-block">
+          <a class="navbar-brand" href="#" style="font-size: 2rem;">CIiT</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </header>
+
+    <div class="main">
+      <div class="mx-auto" style="width:60%">
+        <div class="card-deck" style="width:100%">
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-deck" style="width:100%">
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-deck" style="width:100%">
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-deck" style="width:100%">
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card bg-dark text-light rounded-lg" style="width: 18rem;">
+            <div class="text-center card-image">
+              <img src="./img/a.png" class="card-img-top rounded-lg" alt="Image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ユーザー名</h5>
+              <p class="card-text">今日は良い天気だった。楽しい一日だった</p>
+              <div class="text-center card-view-button">
+                <a href="#" class="btn btn-primary">More about</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap');
+
+.navbar{
+  text-align: center;
+}
+
+.title-block{
+  text-align: center;
+  width: 100%;
+}
+
+.main{
+  padding: 5rem 0;
+}
+
+.body {
+  height: 100%;
+  background: #afafaf;
+  font-family: 'Noto Sans JP', sans-serif;
+}
+
+.card-deck{
+  margin-top: 2rem;
+}
+
+.card-image{
+  margin: 0.5rem;
+}
+
+.card-view-button{
+  margin-top: 2rem;
+}
+</style>
