674ec3ec26cf5da73ea7510d1eb9e255521a4633
Sota Goto <goto-sota0926@g.ecc.u-tokyo.ac.jp>
2021-02-05 15:10:07 +0900

    特徴量としてaccountAgeを追加，また評価関数を改変

--- back/evaluationFunction.py
+++ back/evaluationFunction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
+import datetime
 
 class tweetAnalysis:
     # コンストラクタの引数にjsonファイルのパス
@@ -13,12 +14,12 @@ class tweetAnalysis:
         # json --> pandas.DataFrame
         data = []
         for line in res["statuses"]:
-            data.append([line["id"], line["created_at"], line["full_text"], line["user"]["name"], line["user"]["description"],
+            data.append([line["id"], line["user"]["created_at"], line["full_text"], line["user"]["name"], line["user"]["description"],
                         line["user"]["followers_count"], line["user"]["friends_count"], line["user"]["verified"],
-                        line["user"]["listed_count"], line["favorite_count"], line["retweet_count"]])
+                        line["user"]["listed_count"], line["favorite_count"], line["retweet_count"], line["user"]["statuses_count"]])
 
         self.df = pd.DataFrame(data,
-                    columns=["id", "time", "text", "username", "description", "followers_count", "friends_count", "verified", "listed_count", "favorite_count", "retweet_count"])
+                    columns=["id", "time", "text", "username", "description", "followers_count", "friends_count", "verified", "listed_count", "favorite_count", "retweet_count", "statuses_count"])
 
         # str型から適切な型へ変換
         self.df = self.df.astype({"followers_count" : int})
@@ -64,6 +65,16 @@ class tweetAnalysis:
         sigma = self.df[columnName].std()  # 標準偏差
         self.df[columnName] = self.df[columnName].apply(lambda x : self.standardize(x, mu, sigma))
 
+    # アカウント作成時からアカウント年齢を計算
+    def calculateAccountAge(self, str):
+        month_dict = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
+        year  = int(str[len(str)-4:len(str)])
+        month = month_dict[str[4:7]]
+        day   = int(str[8:10])
+        created_at = datetime.date(year, month, day)
+        dt_now = datetime.date.today()
+        return (dt_now-created_at).days
+
     # 評価値を算出
     def calculateEvaluationValue(self):
         # 各特徴量の上限値の定義
@@ -81,6 +92,8 @@ class tweetAnalysis:
         self.df["text_word_count"] = self.df["text"].apply(lambda x : len(x))
         # プロフィールの「です」「ます」の数
         self.df["description_DesuMasu_count"] = self.df["description"].apply(lambda x : self.countDesuMasu(x))
+        # アカウントが作られてからの日数を計算
+        self.df["account_age"] = self.df["time"].apply(lambda str : self.calculateAccountAge(str))
 
         # 正規化（上限値あり）
         # https://www.datarobot.com/jp/blog/datarobot-finds-false-rumors-on-sns/ より上限値がわかる場合，こちらを採用
@@ -89,14 +102,19 @@ class tweetAnalysis:
         self.normalize_column_withUpper("listed_count", upper_listed_count)
 
         # 正規化（上限値なし）
+        self.normalize_column("followers_count")
         self.normalize_column("text_word_count")
         self.normalize_column("retweet_count")
+        self.normalize_column("statuses_count") # 総ツイート数
         self.normalize_column("description_word_count")
+        self.normalize_column("account_age")
 
         # 評価値 まだ検討中
-        self.df["point"] = 7*self.df["normalized_favorite_count"] + 5*self.df["normalized_ff_rate"] + 4.5*self.df["listed_count"] + 3.5*self.df["normalized_retweet_count"] + 10*self.df["verified"]
+        self.df["point"] = 7*self.df["normalized_favorite_count"] + 5*self.df["normalized_ff_rate"] + 5*self.df["normalized_followers_count"] + 4.5*self.df["normalized_listed_count"] + 3.5*self.df["normalized_retweet_count"] + 10*self.df["verified"] + 3.4*self.df["normalized_account_age"] + 3*self.df["normalized_statuses_count"]
 
         self.standardize_column("point") # 非デマ度（評価値）を偏差値として出力
+
+        self.df = self.df.sort_values('point', ascending=False) # 評価値の大きい順に並び替え (降順)
 
     # data frameのcsv出力
     def outputCsvFile(self):
@@ -110,16 +128,17 @@ class tweetAnalysis:
         # json --> pandas.DataFrame
         data = []
         for line in res["statuses"]:
-            data.append([line["id"], line["created_at"], line["full_text"], line["user"]["name"], line["user"]["description"],
-                        line["user"]["followers_count"], int(line["user"]["friends_count"]), line["user"]["verified"],
-                        line["user"]["listed_count"], line["favorite_count"], line["retweet_count"]])
+            data.append([line["id"], line["user"]["created_at"], line["full_text"], line["user"]["name"], line["user"]["description"],
+                        line["user"]["followers_count"], line["user"]["friends_count"], line["user"]["verified"],
+                        line["user"]["listed_count"], line["favorite_count"], line["retweet_count"], line["user"]["statuses_count"]])
 
         self.df = pd.DataFrame(data,
-                    columns=["id", "time", "text", "username", "description", "followers_count", "friends_count", "verified", "listed_count", "favorite_count", "retweet_count"])
+                    columns=["id", "time", "text", "username", "description", "followers_count", "friends_count", "verified", "listed_count", "favorite_count", "retweet_count", "statuses_count"])
     
 # インスタンスの生成
 tA = tweetAnalysis("test_new.json")
 tA.calculateEvaluationValue()
 tA.outputCsvFile()
+
 # tA.outputCsvFile()
 # tA.initializeTweetDataFrame()
